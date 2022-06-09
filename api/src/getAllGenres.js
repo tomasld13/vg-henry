323b42d26352c7dff4d852ce3c6b44749fb674f4
tomasld13 <tomasld13@gmail.com>
2022-06-09 12:42:22 -0300

    Se soluciona problema de carga de generos

--- api/src/getAllGenres.js
+++ api/src/getAllGenres.js
@@ -2,6 +2,8 @@ const axios = require("axios")
 const { Genre } = require("./db.js")
 
 async function getAllGenres(API_KEY){
+    const existen = Genre.findAll()
+    if(existen.lentgh > 1) return "Ya hay generos cargados"
     try {
         (await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`)).data.results.map(g => Genre.create({id: g.id, name: g.name}))
         console.log("Generos cargados correctamente")
