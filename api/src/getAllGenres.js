9db86c627cb4af09b71e3372d9213eef44845b21
tomasld13 <tomasld13@gmail.com>
2022-06-09 12:53:59 -0300

    Se intenta corregir problema con generos

--- api/src/getAllGenres.js
+++ api/src/getAllGenres.js
@@ -4,11 +4,13 @@ const { Genre } = require("./db.js")
 async function getAllGenres(API_KEY){
     const existen = await Genre.findAll()
     if(existen.lentgh > 1) return "Ya hay generos cargados"
-    try {
-        (await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`)).data.results.map(g => Genre.create({id: g.id, name: g.name}))
-        console.log("Generos cargados correctamente")
-    } catch (error) {
-        console.log(error)
+    else{
+        try {
+            (await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`)).data.results.map(g => Genre.create({id: g.id, name: g.name}))
+            console.log("Generos cargados correctamente")
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
